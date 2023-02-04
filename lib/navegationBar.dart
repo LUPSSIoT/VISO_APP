72ed80f86eb74e0c5595c31a3bc5a1f203101dd1
Vinicius Cruz <vi.pel@hotmail.com.com>
2023-02-04 20:29:09 -0300

    modified:   lib/navegationBar.dart

--- lib/navegationBar.dart
+++ lib/navegationBar.dart
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
@@ -43,3 +44,50 @@ class BotNavBar extends StatelessWidget {
         ]);
   }
 }
+=======
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class BotNavBar extends StatelessWidget {
+  final int indiceAtual;
+  const BotNavBar({super.key, required this.indiceAtual});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 50,
+        selectedFontSize: 15,
+        unselectedFontSize: 15,
+        currentIndex: indiceAtual,
+        onTap: null, //onTabTapped,
+        fixedColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 165, 181, 207),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Início',
+          ),
+          BottomNavigationBarItem(
+            //icon: Icon(Icons.maps_home_work_sharp),
+            //icon: Icon(Icons.villa),
+            icon: Icon(Icons.dashboard_sharp),
+            label: 'Ambientes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.hub),
+            label: 'Objetos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Configurações',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ]);
+  }
+}
+>>>>>>> b6f253ef6ed1860c9432d5c8e1978014f3710992
