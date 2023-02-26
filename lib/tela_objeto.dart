b751a90cb7e7933b9f65919e402ea1150937e8a6
thalialongaray <56452075+thalialongaray@users.noreply.github.com>
2023-02-26 00:54:56 -0300

    Update tela_objeto.dart

--- lib/tela_objeto.dart
+++ lib/tela_objeto.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 
-class TelaObjeto extends StatelessWidget {
-  const TelaObjeto({super.key});
+class TelaObjeto extends StatefulWidget {
+  final String nomeObjeto, fabricante;
+
+  const TelaObjeto(this.nomeObjeto, this.fabricante, {super.key});
+
+  @override
+  State<TelaObjeto> createState() => _TelaObjetoState();
+}
+
+class _TelaObjetoState extends State<TelaObjeto> {
+  bool status = true;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +28,7 @@ class TelaObjeto extends StatelessWidget {
         child: Column(
           children: [
             Container(
-              margin: const EdgeInsets.fromLTRB(15, 15, 25, 0),
+              margin: const EdgeInsets.fromLTRB(15, 15, 25, 15),
               alignment: Alignment.topRight,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -30,8 +39,8 @@ class TelaObjeto extends StatelessWidget {
                     },
                     icon: const Icon(Icons.arrow_back_ios, color: Colors.white),
                   ),
-                  const Text('Objeto',
-                      style: TextStyle(
+                  Text(widget.nomeObjeto,
+                      style: const TextStyle(
                           fontFamily: 'Raleway',
                           color: Colors.white,
                           fontSize: 35)),
@@ -41,31 +50,38 @@ class TelaObjeto extends StatelessWidget {
             
             Center(
               child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: const Color.fromARGB(255, 45, 69, 128),
+                    width: 1.5,
+                  )),
                 alignment: Alignment.topLeft,
-                color: const Color.fromARGB(255, 255, 255, 255),
-                width: 260,
+                width: 350,
                 height: 250,
                 child: Column(
                   children: [
-                    const Center(
-                      child: Text(
-                        "Identificação", // assim não da kkkk mas ta ruim de alinhar
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.fromLTRB(15, 10, 0, 0),
+                      child: const Text(
+                        "Identificação",
                         style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                          fontFamily: 'Raleway',
+                          color: Color.fromARGB(255, 91, 129, 200),
+                          fontSize: 25)
                       ),
                     ),
 
                     Container(
                       alignment: Alignment.topLeft,
-                      padding: const EdgeInsets.all(5),
+                      padding: const EdgeInsets.fromLTRB(25, 5, 0, 0),
                       child: const Text(
-                        "Mac:", // assim não da kkkk mas ta ruim de alinhar
+                        "- MAC Wi-Fi\n\n- Fabricante\n\n- Nome\n\n- Proprietário\n\n- Classe",
                         style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                          fontFamily: 'Raleway',
+                          color: Color.fromARGB(255, 91, 129, 200),
+                          fontSize: 16)
                       ),
                     ),
                   ],
@@ -81,28 +97,54 @@ class TelaObjeto extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.fromLTRB(0, 14, 7, 0),
                       color: const Color.fromARGB(255, 255, 255, 255),
-                      width: 120,
+                      width: 165,
                       height: 120,
-                      child: const Text(
-                        "CIMA ESQ",
-                        style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                      child: Column(
+                        children: [
+                          const Text(
+                            "Status",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontFamily: 'Raleway',
+                              color: Color.fromARGB(255, 91, 129, 200),
+                              fontSize: 25)
+                          ),
+
+                          Container(
+                            margin: const EdgeInsets.all(12),
+                            child: Text(
+                              (status) ? "ATIVO" : "INATIVO",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 236, 85, 135),
+                                fontSize: 20)
+                            ),
+                          ),
+
+                          Switch(
+                            activeColor: const Color.fromARGB(255, 59, 232, 65),
+                            value: status,
+                            onChanged: (value) {
+                              setState(() {
+                                status = value;
+                              });
+                            }),
+                        ],
                       ),
                     ),
 
                     Container(
                       margin: const EdgeInsets.fromLTRB(7, 14, 0, 0),
                       color: const Color.fromARGB(255, 255, 255, 255),
-                      width: 120,
+                      width: 165,
                       height: 120,
                       child: const Text(
-                        "CIMA DIR",
+                        "Confiança",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                          fontFamily: 'Raleway',
+                          color: Color.fromARGB(255, 91, 129, 200),
+                          fontSize: 25)
                       ),
                     ),
                   ],
@@ -114,28 +156,30 @@ class TelaObjeto extends StatelessWidget {
                     Container(
                       margin: const EdgeInsets.fromLTRB(0, 14, 7, 0),
                       color: const Color.fromARGB(255, 255, 255, 255),
-                      width: 120,
+                      width: 165,
                       height: 120,
                       child: const Text(
-                        "BAIXO ESQ",
+                        "Rede",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                          fontFamily: 'Raleway',
+                          color: Color.fromARGB(255, 91, 129, 200),
+                          fontSize: 25)
                       ),
                     ),
 
                     Container(
                       margin: const EdgeInsets.fromLTRB(7, 14, 0, 0),
                       color: const Color.fromARGB(255, 255, 255, 255),
-                      width: 120,
+                      width: 165,
                       height: 120,
                       child: const Text(
-                        "BAIXO DIR",
+                        "Sinal",
+                        textAlign: TextAlign.center,
                         style: TextStyle(
-                          color: Color.fromARGB(255, 0, 0, 0),
-                          fontSize: 30,
-                        ),
+                          fontFamily: 'Raleway',
+                          color: Color.fromARGB(255, 91, 129, 200),
+                          fontSize: 25)
                       ),
                     ),
                   ],
